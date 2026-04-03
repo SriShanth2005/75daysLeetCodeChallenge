520121f39d8ed4df90fbf689e853bab106224e4a
MADDIREDDYGARI SRISHANTH REDDY <maddireddysrishanth@gmail.com>
2026-04-03 21:16:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0;
+        int mid=0;
+        int h=nums.length-1;
+        while(l<=h){
+            mid=(l+h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        return -1;
+    }
+}
