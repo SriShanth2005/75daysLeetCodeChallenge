a7cb12fa7e7d9e6ed501712ea1f113bff6293cf2
MADDIREDDYGARI SRISHANTH REDDY <maddireddysrishanth@gmail.com>
2026-06-03 18:32:29 +0530

    Time: 4 ms (39.65%), Space: 43.7 MB (9.97%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,27 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            } else {
-                if (st.isEmpty()) return false;
-                char top = st.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
+        Stack<Character> st=new Stack<>();
+        if(s.length()==1){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else if(!st.isEmpty() &&
+                   ((ch == ')' && st.peek() == '(') ||
+                    (ch == '}' && st.peek() == '{') ||
+                    (ch == ']' && st.peek() == '[')))
+                     {
+                        st.pop();
+                     }
+            else
+            {
                     return false;
-                }
             }
         }
+       
         return st.isEmpty();
     }
 }
