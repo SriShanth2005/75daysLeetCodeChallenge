d54d3739406db8bbc612ba5bf979da4b0a070969
MADDIREDDYGARI SRISHANTH REDDY <maddireddysrishanth@gmail.com>
2026-07-08 18:35:31 +0530

    Time: 1 ms (98.64%), Space: 47.4 MB (59.63%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int totalsum=0;
-        for(int i=0;i<nums.length;i++){
-            totalsum+=nums[i];
+        int[] leftsum=new int[nums.length];
+        int[] rightsum=new int[nums.length];
+        leftsum[0]=0;
+        for(int i=1;i<nums.length;i++){
+            leftsum[i]=leftsum[i-1]+nums[i-1];
         }
-        int leftsum=0;
-        int rightsum=0;
+        rightsum[nums.length-1]=0;
+        for(int i=nums.length-2;i>=0;i--){
+            rightsum[i]=rightsum[i+1]+nums[i+1];
+        }
         for(int i=0;i<nums.length;i++){
-            rightsum=totalsum-leftsum-nums[i];
-            if(rightsum==leftsum){
+            if(leftsum[i]==rightsum[i]){
                 return i;
             }
-            leftsum+=nums[i];
         }
         return -1;
-        }
     }
+}
