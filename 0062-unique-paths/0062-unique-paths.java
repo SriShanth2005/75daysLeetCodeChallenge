9aeb4551407bd7c8c540ed882fd1a1760f2dd9be
MADDIREDDYGARI SRISHANTH REDDY <maddireddysrishanth@gmail.com>
2026-07-15 19:48:09 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,23 @@
 class Solution {
-    public static int solve(int m,int n,int i,int j,int[][] dp){
-        if(i==m||j==n){
-            return 0;
-        }
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-           int right=solve(m,n,i,j+1,dp);
-           int left=solve(m,n,i+1,j,dp);
-           return dp[i][j]=right+left;
-    }
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++)
+        {
+            dp[i][0] = 1;
         }
-        return solve(m,n,0,0,dp);
+        for(int i = 0; i < n ; i++)
+        {
+            dp[0][i] = 1;
+        }
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
+        
         
     }
 }
